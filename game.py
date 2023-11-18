92de6a16c06ab1cfe96643182ef2c6449b769c90
MatyxCZ <145228987+MatyxCZ@users.noreply.github.com>
2023-11-18 23:35:58 +0100

    Update game.py fixed score bug + game splitted to rounds and after round finishes press spacebar to start a new one

--- game.py
+++ game.py
@@ -9,15 +9,16 @@ pygame.init()
 WIDTH, HEIGHT = 800, 600
 PADDLE_WIDTH, PADDLE_HEIGHT = 10, 100
 BALL_SIZE = 15
-DEFAULT_PADDLE_SPEED = 6  # Default paddle speed
+DEFAULT_PADDLE_SPEED = 6
 PADDLE_SPEED = DEFAULT_PADDLE_SPEED
-DEFAULT_BALL_SPEED = 6  # Default ball speed
+DEFAULT_BALL_SPEED = 6
 BALL_SPEED = DEFAULT_BALL_SPEED
 MAX_SPEED = 20
-DEFAULT_ACCELERATION_FACTOR = 1.15  # Default acceleration factor
+DEFAULT_ACCELERATION_FACTOR = 1.15
 ACCELERATION_FACTOR = DEFAULT_ACCELERATION_FACTOR
 MAX_ACCELERATION_FACTOR = 2.5
-SLOWMO_DURATION = 5 * 60  # 5 seconds in frames
+SLOWMO_DURATION = 5 * 60
+RESET_DELAY = 3 * 60  # 3 seconds in frames before ball reset
 
 # Colors
 WHITE = (255, 255, 255)
@@ -25,7 +26,7 @@ BLACK = (0, 0, 0)
 
 # Create the game window
 win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("PO(G)NG")  # Change the game caption
+pygame.display.set_caption("Pong")
 
 # Paddle objects
 player1 = pygame.Rect(50, HEIGHT // 2 - PADDLE_HEIGHT // 2, PADDLE_WIDTH, PADDLE_HEIGHT)
@@ -33,43 +34,45 @@ player2 = pygame.Rect(WIDTH - 50 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 
 
 # Ball object
 ball = pygame.Rect(WIDTH // 2 - BALL_SIZE // 2, HEIGHT // 2 - BALL_SIZE // 2, BALL_SIZE, BALL_SIZE)
-ball_speed = [BALL_SPEED, BALL_SPEED]  # Ball speed in x and y direction
-ball_angle = random.uniform(-math.pi / 4, math.pi / 4)  # Random angle for initial direction
-ball_speed[0] *= math.cos(ball_angle)
-ball_speed[1] *= math.sin(ball_angle)
+ball_speed = [BALL_SPEED, BALL_SPEED]
+reset_delay = 0  # Countdown before ball reset
 
-# Slow-motion ability variables
-slowmo_active = False
-slowmo_duration = 0
-slowmo_cooldown = 0
-
-# Strike counter
-streak_counter = 1
-font = pygame.font.Font(None, 36)  # Font for streak counter
+# Score variables
+score_player1 = 0
+score_player2 = 0
+font = pygame.font.Font(None, 36)
 
 clock = pygame.time.Clock()
-TICK_RATE_NORMAL = 60  # Normal tick rate (60 frames per second)
-TICK_RATE_SLOW = 30  # Slow tick rate (30 frames per second)
-current_tick_rate = TICK_RATE_NORMAL  # Set initial tick rate to normal
+TICK_RATE_NORMAL = 60
+TICK_RATE_SLOW = 30
+current_tick_rate = TICK_RATE_NORMAL
 
 running = True
 
+def reset_ball():
+    global reset_delay
+    ball.center = (WIDTH // 2, HEIGHT // 2)
+    reset_delay = RESET_DELAY
+
 # Main game loop
 while running:
-    clock.tick(current_tick_rate)  # Set the clock tick based on current_tick_rate
+    clock.tick(current_tick_rate)
     win.fill(BLACK)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        # Activate slow-motion ability
         if event.type == pygame.KEYDOWN and event.key == pygame.K_e:
-            current_tick_rate = TICK_RATE_SLOW  # Change tick rate to slow (30 FPS)
-
-        # Deactivate slow-motion ability
+            current_tick_rate = TICK_RATE_SLOW
         if event.type == pygame.KEYUP and event.key == pygame.K_e:
-            current_tick_rate = TICK_RATE_NORMAL  # Restore tick rate to normal (60 FPS)
+            current_tick_rate = TICK_RATE_NORMAL
+
+        # Check for spacebar to start a new round
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and reset_delay == 0:
+            score_player1 = 0
+            score_player2 = 0
+            reset_ball()
 
     keys = pygame.key.get_pressed()
 
@@ -85,48 +88,47 @@ while running:
         player2.y += PADDLE_SPEED
 
     # Ball movement
-    ball.x += ball_speed[0]
-    ball.y += ball_speed[1]
+    if reset_delay == 0:
+        ball.x += ball_speed[0]
+        ball.y += ball_speed[1]
 
-    # Ball collision with walls (top, bottom, left, right)
-    if ball.top <= 0 or ball.bottom >= HEIGHT:
-        ball_speed[1] *= -1
-    if ball.left <= 0 or ball.right >= WIDTH:
-        ball_speed[0] *= -1
+        # Ball collision with walls
+        if ball.top <= 0 or ball.bottom >= HEIGHT:
+            ball_speed[1] *= -1
 
-    # Ball collision with paddles
-    if ball.colliderect(player1) or ball.colliderect(player2):
-        ball_speed[0] *= -1
-        ball_speed[1] *= random.uniform(0.9, 1.1)  # Randomize bounce off angle slightly
-        streak_counter += 1
-        ball_speed[0] *= ACCELERATION_FACTOR ** streak_counter
-        ball_speed[1] *= ACCELERATION_FACTOR ** streak_counter
+        # Ball collision with paddles
+        if ball.colliderect(player1) or ball.colliderect(player2):
+            ball_speed[0] *= -1
+            ball_speed[1] *= random.uniform(0.9, 1.1)
+            ball_speed[0] *= ACCELERATION_FACTOR
+            ball_speed[1] *= ACCELERATION_FACTOR
 
-        # Limit the maximum ball speed and acceleration factor
-        BALL_SPEED = min(MAX_SPEED, BALL_SPEED)
-        ACCELERATION_FACTOR = min(MAX_ACCELERATION_FACTOR, ACCELERATION_FACTOR)
+            # Limit the maximum ball speed and acceleration factor
+            BALL_SPEED = min(MAX_SPEED, BALL_SPEED)
+            ACCELERATION_FACTOR = min(MAX_ACCELERATION_FACTOR, ACCELERATION_FACTOR)
 
-    # Slow-motion ability logic
-    if slowmo_active:
-        if slowmo_duration > 0:
-            slowmo_duration -= 1
-        else:
-            slowmo_active = False
-            BALL_SPEED = DEFAULT_BALL_SPEED  # Restore default ball speed
-            PADDLE_SPEED = DEFAULT_PADDLE_SPEED  # Restore default paddle speed
+    else:
+        reset_delay -= 1
+        if reset_delay == 0:
+            reset_ball()
 
-    # Reduce slow-motion ability cooldown
-    if slowmo_cooldown > 0:
-        slowmo_cooldown -= 1
+    # Ball out of bounds - scoring
+    if ball.left <= 0:
+        score_player2 += 1
+        reset_ball()
 
-    # Render streak counter text
-    streak_text = font.render(f"Streak: {streak_counter}", True, WHITE)
-    win.blit(streak_text, (WIDTH // 2 - 70, 20))  # Display streak counter on the screen
+    elif ball.right >= WIDTH:
+        score_player1 += 1
+        reset_ball()
 
     # Drawing objects
     pygame.draw.rect(win, WHITE, player1)
     pygame.draw.rect(win, WHITE, player2)
     pygame.draw.ellipse(win, WHITE, ball)
+
+    # Render scores
+    score_text = font.render(f"{score_player1} - {score_player2}", True, WHITE)
+    win.blit(score_text, (WIDTH // 2 - 50, 20))
 
     # Update the display
     pygame.display.flip()
